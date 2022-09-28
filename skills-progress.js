a04cf5569a60d70c330cbab6887e377467380de1
potocki92 <mateusz.potocki92@gmail.com>
2022-09-28 12:28:55 +0200

    new commit: * added comments to "skills-progress.js" * added IntersectionObserver to "skills-progress.js"  (now skills progression show when user scroll to "section__skills")
    * created "contact__form" to "section__contact"
    * added new styles to "style.css" ("FORMS")
    * change "section__title" color

--- skills-progress.js
+++ skills-progress.js
@@ -1,10 +1,23 @@
-$(".skills__outer").each(function () {
-  $(this)
-    .find(".skills__inner")
-    .animate(
-      {
-        width: $(this).attr("data-percent"),
-      },
-      2500
-    );
+// skills progression animation
+
+// if .skills__outer is visible on screen, then animation is start .
+// animation is started once. if you wanna see this again (or you missed), you need to restar the page.
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      $(".skills__outer").each(function () {
+        $(this)
+          .find(".skills__inner")
+          .animate(
+            {
+              width: $(this).attr("data-percent"),
+            },
+            2500
+          );
+      });
+    }
+  });
 });
+
+// tell the observer which elements to track
+observer.observe(document.querySelector(".skills__outer"));
