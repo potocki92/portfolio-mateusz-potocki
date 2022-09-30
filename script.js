e11f5d2c3993ff585fadba5be36563f41c18d910
potocki92 <mateusz.potocki92@gmail.com>
2022-09-30 12:48:42 +0200

    new commit
    script.js:
      * create "header__link" selector in javascript
      * changed eventListener in "toggleBtn"
      * added new lines into "smooth scroll" (closeMenu and remove "shown")
    
    index.html
      * changed "data-wow-delay" from 0.2s to 0.4s

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-
 // initialize wow.js
 new WOW().init();
 
 // animate burger button
 
-const elem = document.querySelector('.burger__background'), 
-      body = document.querySelector('body'),
-      toggleBtn = document.querySelector('.burger__icon'),
-      elemH = elem.getBoundingClientRect().height,
-      elemW = elem.getBoundingClientRect().width;
+const elem = document.querySelector(".burger__background"),
+  body = document.querySelector("body"),
+  toggleBtn = document.querySelector(".burger__icon"),
+  elemH = elem.getBoundingClientRect().height,
+  elemW = elem.getBoundingClientRect().width;
 
 let open = false;
 let scale, offsetX, offsetY;
 
-const calculateValues = (() => {
+const calculateValues = () => {
   const w = window.innerWidth;
   const h = window.innerHeight;
-  const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
+  const offsetValue = Number(
+    getComputedStyle(elem).getPropertyValue("--offset-value")
+  );
 
-  offsetX = (w/2) - (elemW/2) - offsetValue;
-  offsetY = (h/2) - (elemH/2) - offsetValue;
+  offsetX = w / 2 - elemW / 2 - offsetValue;
+  offsetY = h / 2 - elemH / 2 - offsetValue;
 
-  const radius = Math.sqrt((h**2)+(w**2));
-  scale = radius/(elemW/2)/2 + .1;
+  const radius = Math.sqrt(h ** 2 + w ** 2);
+  scale = radius / (elemW / 2) / 2 + 0.1;
   return scale;
-})
+};
 
 const openMenu = () => {
   elem.style.setProperty("--translate-x", `-${offsetX}px`);
   elem.style.setProperty("--translate-y", `${offsetY}px`);
   elem.style.setProperty("--scale", scale);
-}
+};
 const closeMenu = () => {
   elem.style.setProperty("--scale", 1);
   elem.style.setProperty("--translate-x", 0);
   elem.style.setProperty("--translate-y", 0);
-}
+};
 const animateMenu = () => {
   open ? openMenu() : closeMenu();
 };
@@ -43,29 +44,45 @@ const animateMenu = () => {
 const toggleMenu = () => {
   open = !open;
   animateMenu();
-  body.classList.toggle('shown');
-}
+  body.classList.toggle("shown");
+};
 
-const resizeHandler = () => { 
+const resizeHandler = () => {
   window.requestAnimationFrame(() => {
     calculateValues();
     animateMenu();
   });
-}
+};
 
 calculateValues();
 
+// create "shown" class into body
+// hide or visible "header__links" <a> if "shown" class is exist
+var headerLink = document.querySelector(".header__link");
+toggleBtn.addEventListener("click", function () {
+  body.classList.toggle("shown");
+  if (body.classList.contains("shown")) {
+    headerLink.style.visibility = "visible";
+    openMenu();
+  } else {
+    headerLink.style.visibility = "hidden";
+    closeMenu();
+  }
+});
+
 //toggleBtn.onclick = toggleMenu;
-toggleBtn.addEventListener('click', toggleMenu, false);
+//  toggleBtn.addEventListener('click', toggleMenu, false);
 window.addEventListener("resize", resizeHandler, false);
 
 // smooth scroll
 $("a").on("click", function (e) {
   if (this.hash !== "") {
     e.preventDefault();
+    // after click the link menu button is hidden and body class "shown" is remove
+    closeMenu();
+    body.classList.remove("shown");
 
     const hash = this.hash;
-
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top,
@@ -75,15 +92,14 @@ $("a").on("click", function (e) {
   }
 });
 
-
-// typing animation 
+// typing animation
 
 $(document).ready(function () {
   (function ($) {
     $.fn.writeText = function (content) {
       var contentArray = content.split(""),
-      current = 0,
-      elem = this;
+        current = 0,
+        elem = this;
       setInterval(function () {
         if (current < contentArray.length) {
           elem.text(elem.text() + contentArray[current++]);
@@ -93,4 +109,4 @@ $(document).ready(function () {
   })(jQuery);
 
   $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
-})
+});
